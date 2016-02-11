4cdf4f981b6983916c750b6de53c252856613472
vrxfile <vrxfile@mail.ru>
2016-02-11 12:39:30 +0300

    Added tests for servo motors

---
+++ servo_motor_test/servo_motor_test.ino
@@ -0,0 +1,40 @@
+/*
+  Пример работы с сервомоторами
+  Created by Rostislav Varzar
+*/
+
+#include <Servo.h>
+
+#define SERVO1_PWM 9
+#define SERVO2_PWM 10
+
+Servo servo_1;
+Servo servo_2;
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(9600);
+  // Инициализация портов для управления сервомоторами
+  servo_1.attach(SERVO1_PWM);
+  servo_2.attach(SERVO2_PWM);
+}
+
+void loop() {
+  int angle;
+  // Плавно вращаем в одну сторону
+  for (angle = 0 ; angle <= 180; angle += 1)
+  {
+    servo_1.write(angle);
+    servo_2.write(angle);
+    Serial.println("Angle: " + String(angle) + " degrees");
+    delay(50);
+  }
+  // // Плавно вращаем обратно
+  for (angle = 180 ; angle >= 0; angle -= 1)
+  {
+    servo_1.write(angle);
+    servo_2.write(angle);
+    Serial.println("Angle: " + String(angle) + " degrees");
+    delay(50);
+  }
+}
