b66ee577d40a3a8c3afdaced42d6b47870f8290b
vrxfile <vrxfile@mail.ru>
2016-02-18 16:15:39 +0300

    Added DHT11 and Digit Shield test

---
+++ dht11_digit_shield/dht11_digit_shield.ino
@@ -0,0 +1,34 @@
+/*
+  Пример работы с датчиком DHT11 и платой Digit Shield
+  Created by Rostislav Varzar
+*/
+
+#include <DHT.h>
+#include <DigitShield.h>
+
+#define DHTPIN 10
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(9600);
+  // Инициализация цифрового дисплея
+  DigitShield.begin();
+  // Инициализация датчика
+  dht.begin();
+}
+
+void loop() {
+  // Измерение
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  // Вывод температуры
+  DigitShield.setPrecision(1);
+  DigitShield.setValue(t);
+  delay(5000);
+  // Вывод влажности
+  DigitShield.setPrecision(0);
+  DigitShield.setValue(h);
+  delay(5000);
+}
