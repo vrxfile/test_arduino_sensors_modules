192f2a139c2fa90e657225c9bf9ea95f2579170f
vrxfile <vrxfile@mail.ru>
2016-04-01 19:18:55 +0300

    Added some sensors test

---
+++ hcsr501_sensor_test/hcsr501_sensor_test.ino
@@ -0,0 +1,27 @@
+/*
+  Пример работы с ИК датчиком движения HC-SR501
+  Created by Rostislav Varzar
+*/
+
+#define SENSPIN 12
+
+int sensor_data = 0;
+int old_sensor_data = 0;
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(9600);
+  // Инициализация входа цифрового датчика
+  pinMode(SENSPIN, INPUT);
+}
+
+void loop() {
+  // Измерение
+  sensor_data = digitalRead(SENSPIN);
+  if (sensor_data != old_sensor_data)
+  {
+    Serial.println("Motion detected!");
+  }
+  old_sensor_data = sensor_data;
+  delay(10);
+}
