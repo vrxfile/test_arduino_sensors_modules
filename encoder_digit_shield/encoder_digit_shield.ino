095ab07b8ef890017bff78388fac8da199ff3271
vrxfile <vrxfile@mail.ru>
2016-06-09 18:44:29 +0300

    Added motor encoder test with Digit Shield

---
+++ encoder_digit_shield/encoder_digit_shield.ino
@@ -0,0 +1,36 @@
+/*
+  Пример работы с моторным энкодером и платой Digit Shield
+  Created by Rostislav Varzar
+*/
+
+#include <Encoder.h>
+#include <DigitShield.h>
+
+// Объект для работы с энкодером
+Encoder myEnc(10, 11);
+
+// Старое положение энкодера
+long oldPosition  = 0;
+
+// Новое положение энкодера
+long newPosition  = 0;
+
+void setup()
+{
+  // Инициализация цифрового дисплея
+  DigitShield.begin();
+  DigitShield.setPrecision(0);
+  DigitShield.setValue(0);
+}
+
+void loop()
+{
+  // Чтение энкодера
+  newPosition = myEnc.read();
+  // Вывод положения энкодера на дисплей, если оно изменилось
+  if (newPosition != oldPosition)
+  {
+    oldPosition = newPosition;
+    DigitShield.setValue(double(abs(newPosition)));
+  }
+}
