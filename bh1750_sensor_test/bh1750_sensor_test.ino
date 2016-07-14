a27b88da420dc524cf9574c6b27432ff60698ca3
vrxfile <vrxfile@mail.ru>
2016-07-14 11:34:46 +0300

    Added BH1750 light sensor example

---
+++ bh1750_sensor_test/bh1750_sensor_test.ino
@@ -0,0 +1,23 @@
+/*
+  Пример работы с модулем цифрового датчика освещенности BH1750
+  Created by Rostislav Varzar
+*/
+#include <BH1750FVI.h>
+
+BH1750FVI LightSensor_1;
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(9600);
+  // Инициализация датчика
+  LightSensor_1.begin();
+  LightSensor_1.setMode(Continuously_High_Resolution_Mode);
+}
+
+void loop() {
+  // Измерение
+  float l = LightSensor_1.getAmbientLight();
+  // Вывод измеренных значений
+  Serial.println("Ambient light intensity: " + String(l, 1) + " lx");
+  delay(250);
+}
