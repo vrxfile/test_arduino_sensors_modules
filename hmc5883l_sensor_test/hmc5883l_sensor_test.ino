e83886b30ee157b5d803ef15c974cdc098705c98
vrxfile <vrxfile@mail.ru>
2016-07-14 11:17:53 +0300

    Added HMC5883L and RGB matrix examples

---
+++ hmc5883l_sensor_test/hmc5883l_sensor_test.ino
@@ -0,0 +1,33 @@
+/*
+  Пример работы с модулем цифрового компаса HCM5883L
+  Created by Rostislav Varzar
+*/
+#include <Adafruit_HMC5883_U.h>
+
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(15883);
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(9600);
+  // Инициализация датчика HMC5883L
+  if (!mag.begin())
+  {
+    Serial.println("Could not find a valid HMC5883L sensor!");
+  }
+  delay(1000);
+}
+
+void loop() {
+  // Измерение магнитного поля
+  sensors_event_t m_event;
+  mag.getEvent(&m_event);
+  float mx = m_event.magnetic.x;
+  float my = m_event.magnetic.y;
+  float mz = m_event.magnetic.z;
+  // Вывод измеренных значений
+  Serial.println("MX = " + String(mx, 1) + " uT");
+  Serial.println("MY = " + String(my, 1) + " uT");
+  Serial.println("MZ = " + String(mz, 1) + " uT");
+  Serial.println();
+  delay(250);
+}
---
+++ led_matrix_ws2812/led_matrix_ws2812.ino
@@ -0,0 +1,30 @@
+/*
+  Пример работы с RGB драйвером матрицы светодиодов WS2812
+  При использовании данного примера, сигнальный вход матрицы подключать к порту D8
+  Created by Rostislav Varzar
+*/
+#include "HCWS2812.h"
+
+// Количество светодиодов в матрице
+#define NUMBEROFLEDS 64
+
+HCWS2812 HCWS2812_MATRIX;
+
+void setup() {
+  // Очистка выходного буфера
+  HCWS2812_MATRIX.ClearBuffer();
+  // Установка черного цвета фона
+  HCWS2812_MATRIX.SetBG(0, 0, 0);
+}
+
+void loop() {
+  // Установка случайного цвета шрифта
+  HCWS2812_MATRIX.SetFontFG(random(0, 100), random(0, 100), random(0, 100));
+  // Выводим бегущую строку
+  for (int i = 0; i < 48; i++)
+  {
+    HCWS2812_MATRIX.print("MGBOT", i);
+    HCWS2812_MATRIX.Refresh();
+    delay(50);
+  }
+}
