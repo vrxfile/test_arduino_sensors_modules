95dc2b93f8ffb983b51b3450bbe14e59838aedcf
vrxfile <vrxfile@mail.ru>
2016-08-25 17:58:31 +0300

    Added test for E18-D80NK IR sensor

---
+++ digital_ir_sensor_test/digital_ir_sensor_test.ino
@@ -0,0 +1,21 @@
+/*
+  Пример работы с ИК датчиком препятствий E18-D80NK
+  Created by Rostislav Varzar
+*/
+
+#define SENSPIN A0
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(9600);
+  // Инициализация входа цифрового датчика
+  pinMode(SENSPIN, INPUT_PULLUP);
+}
+
+void loop() {
+  // Измерение
+  int sensor_data = digitalRead(SENSPIN);
+  // Вывод измеренных значений
+  Serial.println("Digital IR sensor: " + String(sensor_data));
+  delay(100);
+}
