b729af28316735827c7d1ed8dc5db146c1eec886
vrxfile <vrxfile@mail.ru>
2016-08-25 18:25:01 +0300

    Added test rain sensor and buzzer test

---
+++ buzzer_test/buzzer_test.ino
@@ -0,0 +1,22 @@
+/*
+  Пример управления пьезоэлектрическим динамиком
+  Created by Rostislav Varzar
+*/
+
+#define BUZZERPIN 3
+
+void setup() {
+  // Инициализация выхода динамика
+  pinMode(BUZZERPIN, OUTPUT);
+}
+
+void loop() {
+  // Включение
+  tone(BUZZERPIN, 440, 200);
+  // Пауза
+  delay(1000);
+  // Выключение
+  noTone(BUZZERPIN);
+  // Пауза
+  delay(1000);
+}
