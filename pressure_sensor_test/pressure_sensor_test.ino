2c8747a55be60784dae7f92858524a669f146e9a
vrxfile <vrxfile@mail.ru>
2016-12-15 17:54:06 +0300

    Added new examples for sensors

---
+++ pressure_sensor_test/pressure_sensor_test.ino
@@ -0,0 +1,44 @@
+/*
+  Пример работы с датчиками атмосферного давления BMP085/BMP180
+  Created by Rostislav Varzar
+*/
+#include <Adafruit_BMP085_U.h>
+
+Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
+
+float p, h, t;
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(9600);
+  // Инициализация датчика BMP085/BMP180
+  if (!bmp.begin())
+  {
+    Serial.println("Could not find a valid BMP085 sensor!");
+  }
+  delay(1000);
+}
+
+void loop() {
+  // Измерение давления, высоты и температуры
+  sensors_event_t p_event;
+  bmp.getEvent(&p_event);
+  if (p_event.pressure)
+  {
+    p = p_event.pressure * 7.5006 / 10;
+    bmp.getTemperature(&t);
+    float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
+    h = bmp.pressureToAltitude(seaLevelPressure, p_event.pressure);
+  }
+  // Вывод измеренных значений
+  Serial.print(String(p, 1));
+  Serial.print("\t");
+  Serial.print(String(h, 1));
+  Serial.print("\t");
+  Serial.print(String(t, 1));
+  Serial.print("\t");
+
+  Serial.println();
+
+  delay(100);
+}
---
+++ sound_sensor_graphic/sound_sensor_graphic.ino
@@ -0,0 +1,27 @@
+/*
+  Пример работы с аналоговым датчиком звука (рисование графика в терминале)
+  Created by Rostislav Varzar
+*/
+
+#define SENSPIN A0
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(115200);
+  // Инициализация входа аналогового датчика
+  pinMode(SENSPIN, INPUT);
+}
+
+void loop() {
+  // Измерение
+  int sensor_data = analogRead(SENSPIN);
+  // Расчет количества "плюсиков"
+  int num_symbols = sensor_data / 5 + 1;
+  // Вывод плюсиков в терминал
+  for (int i = 0; i < num_symbols; i ++)
+  {
+    Serial.print("+");
+  }
+  Serial.println();
+  delay(1);
+}
