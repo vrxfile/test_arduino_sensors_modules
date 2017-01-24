fec70012d1a9303b9222a8bf6af5d48a98d6220f
vrxfile <vrxfile@mail.ru>
2017-01-24 16:07:39 +0300

    Added example for AM2320 sensor

---
+++ am2320_sensor_test/am2320_sensor_test.ino
@@ -0,0 +1,28 @@
+/*
+  Пример работы с датчиком температуры/влажности AM2320
+  Created by Rostislav Varzar
+*/
+
+#include <AM2320.h>
+
+AM2320 sensor;
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(9600);
+  // Инициализация датчика
+  sensor.begin();
+}
+
+void loop() {
+  // Измерение
+  if (sensor.measure())
+  {
+    float t = sensor.getTemperature();
+    float h = sensor.getHumidity();
+    // Вывод измеренных значений
+    Serial.println("Temperature: " + String(t, 1) + " *C");
+    Serial.println("Humidity: " + String(h, 1) + " %");
+  }
+  delay(2500);
+}
