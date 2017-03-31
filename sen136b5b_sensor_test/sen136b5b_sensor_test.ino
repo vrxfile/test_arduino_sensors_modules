d30bc3a40107d1e2444af534fbfdaca0157c89e0
vrxfile <vrxfile@mail.ru>
2017-03-31 16:02:18 +0300

    Added 3-pin ultrasonic sensor test

---
+++ sen136b5b_sensor_test/sen136b5b_sensor_test.ino
@@ -0,0 +1,34 @@
+/*
+  Пример работы с УЗ датчиком расстояния HC-SR04
+  Created by Rostislav Varzar
+*/
+
+#define sigPin A5
+int maximumRange = 400;
+int minimumRange = 0;
+long duration = 0;
+long distance = 0;
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Измерение
+  pinMode(sigPin, OUTPUT);
+  digitalWrite(sigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(sigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sigPin, LOW);
+  pinMode(sigPin, INPUT);
+  duration = pulseIn(sigPin, HIGH);
+  distance = duration / 58.2;
+  if (distance >= maximumRange || distance <= minimumRange) {
+    distance = -1;
+  }
+  // Вывод измеренных значений
+  Serial.println("Distance: " + String(distance) + " cm");
+  delay(100);
+}
