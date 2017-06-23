7fea04bd66f193058d899563cd7622f9782fa14e
vrxfile <vrxfile@mail.ru>
2017-06-23 10:37:07 +0300

    Added capacitive moisture sensor test

---
+++ capacitive_moisture_test/capacitive_moisture_test.ino
@@ -0,0 +1,27 @@
+/*
+  Пример работы с датчиком влажности почвы ёмкостным
+  Created by Rostislav Varzar
+*/
+
+#define SENSPIN A0
+const float air_value = 545.0;
+const float water_value = 292.0;
+const float moisture_0 = 0.0;
+const float moisture_100 = 100.0;
+
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(9600);
+  // Инициализация входа аналогового датчика
+  pinMode(SENSPIN, INPUT);
+}
+
+void loop() {
+  // Измерение
+  float sensor_data = analogRead(SENSPIN);
+  float moisture = map(sensor_data, air_value, water_value, moisture_0, moisture_100);
+  // Вывод измеренных значений
+  Serial.println("Moisture sensor: " + String(moisture) + " %");
+  delay(100);
+}
