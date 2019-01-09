3f766c3847ebf50fab01d395bd8d75503603ce96
vrxfile <vrxfile@mail.ru>
2019-01-09 12:31:00 +0300

    Added modules tests

---
+++ lcd_i2c_12864_test/lcd_i2c_12864_test.ino
@@ -0,0 +1,32 @@
+/*
+  Пример работы с графическим модулем LCD 128x64
+  Created by Rostislav Varzar
+*/
+
+#include <Wire.h>
+#include <I2C_graphical_LCD_display.h>
+
+I2C_graphical_LCD_display lcd;
+
+void setup ()
+{
+  // Инициализация дисплея
+  lcd.begin();
+
+  // Рисование рамки
+  lcd.frameRect (0, 0, 127, 63, 1, 1);
+
+  // Рисование квадратика
+  lcd.clear (10, 10, 20, 20, 0xFF);
+
+  // Вывод строки
+  lcd.gotoxy (10, 50);
+  lcd.string ("Privet, MGBOT!", false);
+}
+
+void loop ()
+{
+}
+
+
+
