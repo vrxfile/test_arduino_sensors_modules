1a4d90f334aa314dc802b17636732b286afb5e64
vrxfile <vrxfile@mail.ru>
2019-04-03 13:50:30 +0300

    Added some modules tests

---
+++ as3935_sensor_test/as3935_sensor_test.ino
@@ -0,0 +1,79 @@
+/*
+  Пример работы с модулем датчика грозовых разрядов AS3935
+  Created by Rostislav Varzar
+*/
+#include <Wire.h>
+
+#define sensor_addr 0x03 // Without jumpers
+
+float energy_data = 0;
+float dist_data   = 0;
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(115200);
+  // Инициализация датчика
+  init_sensor();
+  delay(500);
+}
+
+void loop() {
+  // Измерение
+  poll_sensor();
+  // Вывод измеренных значений в терминал
+  Serial.println("ENERGY   = " + String(energy_data, 1));
+  Serial.println("DISTANCE = " + String(dist_data, 1));
+  delay(350);
+}
+
+// Инициализация датчика
+void init_sensor() {
+  Wire.begin();
+  Wire.beginTransmission(sensor_addr);
+  Wire.write(0x00);       // Регистр управления усилителем и спящим режимом
+  Wire.write(0b00100100); // Активный режим, AFE = 0b10010
+  Wire.endTransmission();
+  Wire.beginTransmission(sensor_addr);
+  Wire.write(0x01);       // Регистр шумоподавления и watchdog
+  Wire.write(0b00100010); // NFL = 0b010, WD = 0b0010
+  Wire.endTransmission();
+  Wire.beginTransmission(sensor_addr);
+  Wire.write(0x02);       // Регистр настройки распознавания сигнала
+  Wire.write(0b11000010); // Минимальное количество импульсов = 0, уменьшение чувсвтительности к искрам 0b0010
+  Wire.endTransmission();
+  Wire.beginTransmission(sensor_addr);
+  Wire.write(0x03);       // Регистр настройки антенны
+  Wire.write(0b00000000); // По умолчанию всё "0"
+  Wire.endTransmission();
+  Wire.beginTransmission(sensor_addr);
+  Wire.write(0x08);       // Регистр настройки конденсатора колебательного контура
+  Wire.write(0b00000000); // По умолчанию всё "0"
+  Wire.endTransmission();
+  // Или установка всего по умолчанию
+  Wire.beginTransmission(sensor_addr);
+  Wire.write(0x3C);
+  Wire.write(0x96);
+  Wire.endTransmission();
+  Wire.beginTransmission(sensor_addr);
+  Wire.write(0x3D);
+  Wire.write(0x96);
+  Wire.endTransmission();
+}
+
+// Получение данных с датчика
+void poll_sensor() {
+  unsigned int sensor_data[4];
+  Wire.beginTransmission(sensor_addr);
+  Wire.write(0x04); // Регистр данных
+  Wire.endTransmission();
+  Wire.requestFrom(sensor_addr, 4);
+  if (Wire.available() == 4) {
+    sensor_data[0] = Wire.read();
+    sensor_data[1] = Wire.read();
+    sensor_data[2] = Wire.read();
+    sensor_data[3] = Wire.read();
+  }
+  energy_data = (float)sensor_data[2] * 65536.0 + (float)sensor_data[1] * 256.0 + (float)sensor_data[0];
+  dist_data = (float)sensor_data[3];
+}
+
---
+++ pca9685_driver_test/pca9685_driver_test.ino
@@ -0,0 +1,101 @@
+/*
+  Пример работы с ШИМ драйвером PCA9685
+  Created by Rostislav Varzar
+*/
+#include <Wire.h>
+
+#define driver_addr 0x20
+
+uint8_t m_ctl = 0b00000000;
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(115200);
+  // Инициализация датчика
+  init_driver();
+  delay(500);
+  motor1_backward(); delay(500);
+  motor1_forward(); delay(500);
+  motor1_stop(); delay(500);
+  motor2_backward(); delay(500);
+  motor2_forward(); delay(500);
+  motor2_stop(); delay(500);
+  motor1_backward(); motor2_backward(); delay(500);
+  motor1_forward(); motor2_forward(); delay(500);
+  motor1_stop(); motor2_stop(); delay(500);
+}
+
+void loop() {
+}
+
+// Инициализация драйвера
+void init_driver() {
+  Wire.begin();
+  Wire.setClock(100000L);
+  Wire.beginTransmission(driver_addr);
+  Wire.write(0x00);       // Регистр режима работы
+  Wire.write(0b00100000); // Выход прерывания и отклик от разных адресов отключены
+  Wire.endTransmission();
+  Wire.beginTransmission(driver_addr);
+  Wire.write(0x03);       // Конфигурационный регистр
+  Wire.write(0b00001111); // P0 - P3 настроены как входы
+  Wire.endTransmission();
+}
+
+void motor1_backward()
+{
+  m_ctl = m_ctl & 0b11111100;
+  m_ctl = m_ctl | 0b00000001;
+  Wire.beginTransmission(driver_addr);
+  Wire.write(0x0B);
+  Wire.write(m_ctl);
+  Wire.endTransmission();
+}
+
+void motor1_forward()
+{
+  m_ctl = m_ctl & 0b11111100;
+  m_ctl = m_ctl | 0b00000010;
+  Wire.beginTransmission(driver_addr);
+  Wire.write(0x0B);
+  Wire.write(m_ctl);
+  Wire.endTransmission();
+}
+
+void motor1_stop()
+{
+  m_ctl = m_ctl & 0b11111100;
+  Wire.beginTransmission(driver_addr);
+  Wire.write(0x0B);
+  Wire.write(m_ctl);
+  Wire.endTransmission();
+}
+
+void motor2_backward()
+{
+  m_ctl = m_ctl & 0b11110011;
+  m_ctl = m_ctl | 0b00000100;
+  Wire.beginTransmission(driver_addr);
+  Wire.write(0x0B);
+  Wire.write(m_ctl);
+  Wire.endTransmission();
+}
+
+void motor2_forward()
+{
+  m_ctl = m_ctl & 0b11110011;
+  m_ctl = m_ctl | 0b00001000;
+  Wire.beginTransmission(driver_addr);
+  Wire.write(0x0B);
+  Wire.write(m_ctl);
+  Wire.endTransmission();
+}
+
+void motor2_stop()
+{
+  m_ctl = m_ctl & 0b11110011;
+  Wire.beginTransmission(driver_addr);
+  Wire.write(0x0B);
+  Wire.write(m_ctl);
+  Wire.endTransmission();
+}
